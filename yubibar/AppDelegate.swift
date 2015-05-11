7ce162a56fd989588a351a606d56d7bbadc8055e
Don Goodman-Wilson <dgoodman@railstars.com>
2015-05-11 09:55:40 -0700

    Some logic for making this a menubar app

--- yubibar/AppDelegate.swift
+++ yubibar/AppDelegate.swift
@@ -9,19 +9,52 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
-
+class AppDelegate: NSObject, NSApplicationDelegate, YBStatusItemViewDelegate {
+    
+    let statusBar = NSStatusBar.systemStatusBar()
+    var statusBarItem : NSStatusItem = NSStatusItem()
+    var icon: YBStatusItemView?
+    
+    @IBOutlet weak var mainWindow: NSWindow?
+    
+    override func awakeFromNib() {
+        //Add statusBarItem
+        statusBarItem = statusBar.statusItemWithLength(-1) //NSVariableStatusItemLength
+        self.icon = YBStatusItemView(image: NSImage(named:"statusbaricon")!, item: statusBarItem, delegate: self);
+        statusBarItem.view = self.icon;
+        
+        //Suppress display of icon in dock.
+        NSApp.setActivationPolicy(NSApplicationActivationPolicy.Accessory)
+    }
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func onItemClicked(active: Bool) {
+        if(active == true)
+        {
+            let iconFrame = self.statusBarItem.view!.window!.frame
+            let windowFrame = self.mainWindow!.frame
+            let windowFrameSize = windowFrame.size
+            
+            var origin = NSMakePoint(NSMidX(iconFrame), NSMinY(iconFrame))
+            
+            origin.x -= windowFrameSize.width / 2
+            origin.y -= windowFrameSize.height + 3
+            
+            mainWindow?.setFrameOrigin(origin)
+            
+            mainWindow?.makeKeyAndOrderFront(self)
+            NSApp.activateIgnoringOtherApps(true)
+        }
+        else
+        {
+            mainWindow?.orderOut(self)
+        }
+    }
 }
-
