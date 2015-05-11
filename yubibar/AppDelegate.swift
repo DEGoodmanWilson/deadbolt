071623cfb4d10b8eed048d6fe70ebcb60ef8ece1
Don Goodman-Wilson <dgoodman@railstars.com>
2015-05-11 09:39:43 -0700

    First project files

---
+++ yubibar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  yubibar
+//
+//  Created by D.E. Goodman-Wilson on 5/11/15.
+//  Copyright (c) 2015 D.E. Goodman-Wilson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
